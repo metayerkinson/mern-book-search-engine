a9c2ecb3c32191f5d2aec7a7213cd85ac0de4e74
Your Name <metayerkinson@gmail.com>
2023-08-08 22:41:28 -0400

    updated serv.js file

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const db = require("./config/connection");
+const DB = require("./config/connection");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
@@ -28,7 +28,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-db.once("open", () => {
+// start the server
+DB.once('open', () => {
   app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
